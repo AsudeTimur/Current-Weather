26b7009efae19cb2aeab2f09b659297fd8e6f27f
ZeynepAksoy <zaksoy852@gmail.com>
2024-05-03 16:08:16 +0300

    Added to the weather map of all cities in Turkey

--- app.py
+++ app.py
@@ -1,68 +1,99 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import requests
 import plotly.graph_objs as go
 
 app = Flask(__name__)
 
-def get_weather_by_coordinates(latitude, longitude):
+def get_weather(city):
     API_KEY = '3ea9ff514429a6caa63af98b9e11c9ea'
-    url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=metric'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
     response = requests.get(url)
     
     if response.status_code == 200:
         data = response.json()
         temperature = data['main']['temp']
-        weather_description = data['weather'][0]['description']
-        return temperature, weather_description
+        weather_description = translate_weather_description(data['weather'][0]['description'])
+        longitude = data['coord']['lon']
+        latitude = data['coord']['lat']
+        return temperature, weather_description, longitude, latitude
     else:
-        return None, None
+        return None, None, None, None
 
-def get_all_cities():
-    # Burada tüm dünya şehirlerinin koordinatlarını alacak bir yöntem olmalıdır.
-    # Bu örnek için sabit koordinatlar kullanalım.
-    # Dünya genelinde rastgele birkaç şehir için örnek koordinatlar:
-    cities = [
-        {'name': 'New York', 'latitude': 40.7128, 'longitude': -74.0060},
-        {'name': 'London', 'latitude': 51.5074, 'longitude': -0.1278},
-        {'name': 'Tokyo', 'latitude': 35.6895, 'longitude': 139.6917}
+def translate_weather_description(description):
+    translations = {
+        'clear sky': 'açık hava',
+        'few clouds': 'az bulutlu',
+        'scattered clouds': 'parçalı bulutlu',
+        'broken clouds': 'kırık bulutlu',
+        'overcast clouds': 'kapalı bulutlu',
+        'light rain': 'hafif yağmur',
+        'moderate rain': 'orta şiddetli yağmur',
+        'heavy intensity rain': 'yoğun yağış',
+        'shower rain': 'sağanak yağış',
+        'thunderstorm': 'gök gürültülü fırtına',
+        'snow': 'kar',
+        'mist': 'sis',
+        'smoke': 'duman',
+        'haze': 'puslu',
+        'sand/ dust whirls': 'kum/ toz hortumu',
+        'fog': 'sisli',
+        'sand': 'kumlu',
+        'dust': 'tozlu',
+        'volcanic ash': 'volkanik kül',
+        'squalls': 'şiddetli fırtına',
+        'tornado': 'tornado'
+        # İhtiyacınıza göre diğer hava durumu terimlerini buraya ekleyebilirsiniz
+    }
+    return translations.get(description, description)
+
+def generate_weather_map():
+    turkish_cities = [
+        'Adana', 'Adıyaman', 'Afyonkarahisar', 'Ağrı', 'Amasya', 'Ankara', 'Antalya', 'Artvin', 'Aydın', 'Balıkesir',
+        'Bilecik', 'Bingöl', 'Bitlis', 'Bolu', 'Burdur', 'Bursa', 'Çanakkale', 'Çankırı', 'Çorum', 'Denizli',
+        'Diyarbakır', 'Edirne', 'Elazığ', 'Erzincan', 'Erzurum', 'Eskişehir', 'Gaziantep', 'Giresun', 'Gümüşhane',
+        'Hakkari', 'Hatay', 'Isparta', 'Mersin', 'İstanbul', 'İzmir', 'Kars', 'Kastamonu', 'Kayseri', 'Kırklareli',
+        'Kırşehir', 'Kocaeli', 'Konya', 'Kütahya', 'Malatya', 'Manisa', 'Kahramanmaraş', 'Mardin', 'Muğla', 'Muş',
+        'Nevşehir', 'Niğde', 'Ordu', 'Rize', 'Sakarya', 'Samsun', 'Siirt', 'Sinop', 'Sivas', 'Tekirdağ', 'Tokat',
+        'Trabzon', 'Tunceli', 'Şanlıurfa', 'Uşak', 'Van', 'Yozgat', 'Zonguldak', 'Aksaray', 'Bayburt', 'Karaman',
+        'Kırıkkale', 'Batman', 'Şırnak', 'Bartın', 'Ardahan', 'Iğdır', 'Yalova', 'Karabük', 'Kilis', 'Osmaniye',
+        'Düzce'
     ]
-    return cities
+
+    figure = go.Figure()
+
+    for city in turkish_cities:
+        temperature, weather_description, longitude, latitude = get_weather(city)
+        if temperature is not None and weather_description is not None:
+            figure.add_trace(go.Scattermapbox(
+                lat=[latitude],
+                lon=[longitude],
+                mode='markers',
+                marker=go.scattermapbox.Marker(
+                    size=10,
+                    color='blue',
+                    opacity=0.7
+                ),
+                text=f'{city}<br>Sıcaklık: {temperature}°C<br>Açıklama: {weather_description}',
+                name=city
+            ))
+
+    figure.update_layout(
+        title='Türkiye\'deki Şehirlerin Hava Durumu Haritası',
+        mapbox=dict(
+            style='carto-positron',
+            zoom=5,
+            center=dict(lat=39.9255, lon=32.8667)
+        ),
+        width=1400,
+        height=800
+    )
+
+    return figure
 
 @app.route('/')
 def index():
-    cities = get_all_cities()
-    
-    # Hava durumu verilerini göstermek için bir Scattermapbox grafiği oluşturma
-    figure = go.Figure()
-    
-    for city in cities:
-        city_name = city['name']
-        latitude = city['latitude']
-        longitude = city['longitude']
-        temperature, weather_description = get_weather_by_coordinates(latitude, longitude)
-        
-        figure.add_trace(go.Scattermapbox(
-            lat=[latitude],
-            lon=[longitude],
-            mode='markers',
-            marker=go.scattermapbox.Marker(
-                size=14
-            ),
-            text=[f'{city_name}: {temperature}°C, {weather_description}'],
-            name=city_name
-        ))
-
-    figure.update_layout(
-        title='Hava Durumu Haritası',
-        mapbox=dict(
-            accesstoken='pk.eyJ1IjoiemV5bmVwYWtzb3kiLCJhIjoiY2x2MnRxcWVtMG05NDJubzFkbzNzdmg5YyJ9.qZVpgdncIf3bgQRFn8AOCg',  # Plotly'de bir hesap oluşturarak elde edebilirsiniz
-            zoom=2
-        ),
-        width=1400,  # Genişlik ayarı
-        height=900  # Yükseklik ayarı
-    )
-
-    return render_template('index.html', plot=figure.to_html(full_html=False))
+    weather_map = generate_weather_map()
+    return render_template('index.html', plot=weather_map.to_html(full_html=False))
 
 if __name__ == '__main__':
     app.run(debug=True)
