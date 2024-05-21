c9e1dffa1221604be75bf95d3de2e5e1fdd86a77
Asude Timur <asudetimu@gmail.com>
2024-05-21 13:25:14 +0300

    Revert "implemented feature to persist API data into the database in the project"
    This reverts commit 5036e07d3493e063ae50cd67ad40ec563cefcbe7.

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template
 import requests
-from pymongo import MongoClient
-import threading
-import time
-from bson import json_util
-import json
+from folium import plugins
+import folium
+import numpy as np
 
 app = Flask(__name__)
-
-client = MongoClient('mongodb+srv://zaksoy852:zeynep23@cluster0.h9amh6f.mongodb.net/')
-db = client.hava_veri
-collection = db.hava_durum
 
 def get_weather(city):
     API_KEY = '3ea9ff514429a6caa63af98b9e11c9ea'
@@ -20,75 +14,56 @@ def get_weather(city):
     if response.status_code == 200:
         data = response.json()
         temperature = data['main']['temp']
-        weather_description = translate_weather_description(data['weather'][0]['description'])
         longitude = data['coord']['lon']
         latitude = data['coord']['lat']
-        
-        weather_data = {
-            'city': city,
-            'temperature': temperature,
-            'description': weather_description,
-            'longitude': longitude,
-            'latitude': latitude
-        }
-        collection.update_one({'city': city}, {'$set': weather_data}, upsert=True)
-        
-        return temperature, weather_description, longitude, latitude
+        return temperature, longitude, latitude
     else:
-        return None, None, None, None
-
-def translate_weather_description(description):
-    translations = {
-        'clear sky': 'açık hava',
-        'few clouds': 'az bulutlu',
-        'scattered clouds': 'parçalı bulutlu',
-        'broken clouds': 'kırık bulutlu',
-        'overcast clouds': 'kapalı bulutlu',
-        'light rain': 'hafif yağmur',
-        'moderate rain': 'orta şiddetli yağmur',
-        'heavy intensity rain': 'yoğun yağış',
-        'shower rain': 'sağanak yağış',
-        'thunderstorm': 'gök gürültülü fırtına',
-        'snow': 'kar',
-        'mist': 'sis',
-        'smoke': 'duman',
-        'haze': 'puslu',
-        'sand/ dust whirls': 'kum/ toz hortumu',
-        'fog': 'sisli',
-        'sand': 'kumlu',
-        'dust': 'tozlu',
-        'volcanic ash': 'volkanik kül',
-        'squalls': 'şiddetli fırtına',
-        'tornado': 'tornado'
-    }
-    return translations.get(description, description)
-
-def update_weather_data():
-    while True:
-        turkish_cities = [
-            'Adana', 'Adıyaman', 'Afyonkarahisar', 'Ağrı', 'Amasya', 'Ankara', 'Antalya', 'Artvin', 'Aydın', 'Balıkesir',
-            'Bilecik', 'Bingöl', 'Bitlis', 'Bolu', 'Burdur', 'Bursa', 'Çanakkale', 'Çankırı', 'Çorum', 'Denizli',
-            'Diyarbakır', 'Edirne', 'Elazığ', 'Erzincan', 'Erzurum', 'Eskişehir', 'Gaziantep', 'Giresun', 'Gümüşhane',
-            'Hakkari', 'Hatay', 'Isparta', 'Mersin', 'İstanbul', 'İzmir', 'Kars', 'Kastamonu', 'Kayseri', 'Kırklareli',
-            'Kırşehir', 'Kocaeli', 'Konya', 'Kütahya', 'Malatya', 'Manisa', 'Kahramanmaraş', 'Mardin', 'Muğla', 'Muş',
-            'Nevşehir', 'Niğde', 'Ordu', 'Rize', 'Sakarya', 'Samsun', 'Siirt', 'Sinop', 'Sivas', 'Tekirdağ', 'Tokat',
-            'Trabzon', 'Tunceli', 'Şanlıurfa', 'Uşak', 'Van', 'Yozgat', 'Zonguldak', 'Aksaray', 'Bayburt', 'Karaman',
-            'Kırıkkale', 'Batman', 'Şırnak', 'Bartın', 'Ardahan', 'Iğdır', 'Yalova', 'Karabük', 'Kilis', 'Osmaniye',
-            'Düzce'
-        ]
-        
-        for city in turkish_cities:
-            get_weather(city)
-        
-        time.sleep(300)  # 5 dakika bekle
+        return None, None, None
 
 @app.route('/')
 def index():
-    weather_data = list(collection.find())
-    weather_data_json = json.loads(json_util.dumps(weather_data))
-    return render_template('index.html', weather_data=weather_data_json)
+    turkish_cities = [
+        'Adana', 'Adıyaman', 'Afyonkarahisar', 'Ağrı', 'Amasya', 'Ankara', 'Antalya', 'Artvin', 'Aydın', 'Balıkesir',
+        'Bilecik', 'Bingöl', 'Bitlis', 'Bolu', 'Burdur', 'Bursa', 'Çanakkale', 'Çankırı', 'Çorum', 'Denizli',
+        'Diyarbakır', 'Edirne', 'Elazığ', 'Erzincan', 'Erzurum', 'Eskişehir', 'Gaziantep', 'Giresun', 'Gümüşhane',
+        'Hakkari', 'Hatay', 'Isparta', 'Mersin', 'İstanbul', 'İzmir', 'Kars', 'Kastamonu', 'Kayseri', 'Kırklareli',
+        'Kırşehir', 'Kocaeli', 'Konya', 'Kütahya', 'Malatya', 'Manisa', 'Kahramanmaraş', 'Mardin', 'Muğla', 'Muş',
+        'Nevşehir', 'Niğde', 'Ordu', 'Rize', 'Sakarya', 'Samsun', 'Siirt', 'Sinop', 'Sivas', 'Tekirdağ', 'Tokat',
+        'Trabzon', 'Tunceli', 'Şanlıurfa', 'Uşak', 'Van', 'Yozgat', 'Zonguldak', 'Aksaray', 'Bayburt', 'Karaman',
+        'Kırıkkale', 'Batman', 'Şırnak', 'Bartın', 'Ardahan', 'Iğdır', 'Yalova', 'Karabük', 'Kilis', 'Osmaniye',
+        'Düzce'
+    ]
+
+    weather_data = []
+    for city in turkish_cities:
+        temperature, lon, lat = get_weather(city)
+        if temperature is not None:
+            weather_data.append({
+                'temperature': temperature,
+                'lon': lon,
+                'lat': lat
+            })
+
+    # Create a map centered at Turkey's coordinates
+    map = folium.Map(location=[39.9334, 32.8597], zoom_start=6)
+
+    # Create a HeatMap layer using temperature data
+    heat_data = [[data['lat'], data['lon'], data['temperature']] for data in weather_data]
+
+    # Normalize temperature data to range between 0 and 1
+    temperatures = [data['temperature'] for data in weather_data]
+    normalized_temperatures = np.interp(temperatures, (min(temperatures), max(temperatures)), (0, 1))
+
+    # Combine latitudes, longitudes, and normalized temperatures
+    heat_data_normalized = [[data[0], data[1], normalized_temperatures[i]] for i, data in enumerate(heat_data)]
+
+    # Create HeatMap with gradient
+    plugins.HeatMap(heat_data_normalized, gradient={0.2: 'blue', 0.4: 'cyan', 0.6: 'yellow', 1: 'red'}, radius=15).add_to(map)
+
+    # Save the map as HTML file
+    map.save('templates/heatmap.html')
+
+    return render_template('heatmap.html')
 
 if __name__ == '__main__':
-    weather_update_thread = threading.Thread(target=update_weather_data)
-    weather_update_thread.start()
     app.run(debug=True)
